9c78b6cb599439b66766c99de3f73ee37ade5753
Andrei <a.parchukov@yandex.ru>
2022-10-19 23:58:46 +0300

    Initial Commit

---
+++ SmartHome/SmartHomeApp.swift
@@ -0,0 +1,20 @@
+//
+//  SmartHomeApp.swift
+//  SmartHome
+//
+//  Created by Андрей Парчуков on 19.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
